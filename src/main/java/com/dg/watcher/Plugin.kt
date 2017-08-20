46e0a8a25034663c594f6fac824062bb65417511
Daniel Gronau <xgleich1@gmail.com>
2017-08-20 21:13:40 +0200

    Configured the pom to work shen java code calls kotlin code (compile order); Converted the plugin class to kotlin

---
+++ src/main/java/com/dg/watcher/Plugin.kt
@@ -0,0 +1,34 @@
+package com.dg.watcher
+
+import com.dg.watcher.history.History
+import com.dg.watcher.watching.ApkWatching.watchApkSize
+import hudson.Launcher
+import hudson.model.AbstractBuild
+import hudson.model.AbstractProject
+import hudson.model.BuildListener
+import hudson.tasks.BuildStepMonitor.NONE
+import hudson.tasks.Notifier
+import org.kohsuke.stapler.DataBoundConstructor
+
+
+class Plugin @DataBoundConstructor constructor(val thresholdInMb: Float, val customPathToApk: String) : Notifier() {
+    override fun getDescriptor() = super.getDescriptor() as PluginDescriptor
+
+    override fun getRequiredMonitorService() = NONE
+
+    override fun getProjectAction(project: AbstractProject<*, *>) = History(project)
+
+    override fun perform(build: AbstractBuild<*, *>, launcher: Launcher, listener: BuildListener): Boolean {
+        val buildPermitted = watchApkSize(build, listener.logger, thresholdInMb, customPathToApk)
+
+        updateActiveProjectAction(build.getProject())
+
+        return buildPermitted
+    }
+
+    private fun updateActiveProjectAction(project: AbstractProject<*, *>) =
+            getActiveProjectAction(project).updateHistory()
+
+    private fun getActiveProjectAction(project: AbstractProject<*, *>) =
+            project.getAction(History::class.java)
+}
